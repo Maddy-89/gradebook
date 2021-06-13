590e803e048f01d26cc394a5efddf37768f23e35
Maddy B <maddx@live.com.au>
2021-06-13 18:33:42 +0800

    WriteLineToConsole&UseIfStatementErrorHandling

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,9 +4,21 @@ namespace GradeBook
 {
     class Program
     {
+        // A Method Named Main
+        // Main has a Parameter called args with type string array
+        // array is a collection of objects
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // An If statement to check that the args array is not empty
+            if (args.Length > 0) 
+            {
+                // String Concatonation
+                Console.WriteLine("Hello" + args[0] + "!");
+                // String Interpulation
+                Console.WriteLine($"Hello, {args[0]} !");
+            } else {
+                Console.WriteLine("Hello");
+            }
         }
     }
 }
