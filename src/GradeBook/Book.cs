be97c301e161a8df08b2690a72ece220a64de3c7
Maddy B <maddx@live.com.au>
2021-06-14 19:46:37 +0800

    Defining a Method called AddGrade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,8 @@
 namespace GradeBook
 {
     // Create a new Class called Book
-    // When creating a new class. think about how can this class be the right abstraction for what i need.
+    // Define a Method called AddGrade
+    
     // What that means is how can i give the class the right members so other developers can understand what it si doing.
     // Add Members - What it can do
     // State, or data the class holds 
@@ -9,6 +10,9 @@ namespace GradeBook
     
     class Book
     {
+        public void AddGrade(double grade)
+        {
 
+        }
     }
 }
