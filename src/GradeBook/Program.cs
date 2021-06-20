84469e2251580afc377e74b38ec52fc71a8cc15d
Maddy B <maddx@live.com.au>
2021-06-20 23:28:56 +0800

    fix syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -33,7 +33,7 @@ namespace GradeBook
                 }
                 finally
                 {
-                    Console.WriteLine("**")
+                    Console.WriteLine("**");
                 }
             }
             
