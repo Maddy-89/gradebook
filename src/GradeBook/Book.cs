b7285974273bec8951f21277dfe1c39179f24439
Maddy B <maddx@live.com.au>
2021-06-20 19:54:05 +0800

    Pattern Matching with Switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -29,7 +29,7 @@ namespace GradeBook
                 case 'A':
                     AddGrade(90);
                     break;
-                    
+
                 case 'B':
                     AddGrade(80);
                     break;
@@ -75,6 +75,28 @@ namespace GradeBook
             };
             //Compute an Average/highest/lowest for the number stored in the Grades List
             result.Average /= grades.Count;
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                result.Letter = 'A';
+                break;
+
+                case var d when d >= 80.0:
+                result.Letter = 'B';
+                break;
+
+                case var d when d >= 70.0:
+                result.Letter = 'C';
+                break;
+
+                case var d when d >= 60.0:
+                result.Letter = 'D';
+                break;
+
+                default:
+                result.Letter = 'F';
+                break;
+            }
             return result;
 
         }
