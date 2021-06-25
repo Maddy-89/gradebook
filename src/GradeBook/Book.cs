7d057ece85f69fc97c5128419fb90bd65823fdfc
Maddy B <maddx@live.com.au>
2021-06-25 21:19:58 +0800

    Defining an Interface

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -17,13 +17,27 @@ namespace GradeBook
             set;
         }
     }
-    public abstract class Book : NamedObject
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NamedObject, IBook
     {
         protected Book(string name) : base(name)
         {
         }
 
+        public virtual event GradeAddedDelegate GradeAdded;
+
         public abstract void AddGrade(double grade);
+
+        public virtual Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
     }
     public class InMemoryBook : Book
     {
@@ -68,9 +82,9 @@ namespace GradeBook
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
-        public event GradeAddedDelegate GradeAdded;
+        public override event GradeAddedDelegate GradeAdded;
 // Add statistics class identifier (return an object of type statistics)
-        public Statistics GetStatistics()
+        public override Statistics GetStatistics()
         {
             var result = new Statistics();
             result.Average = 0.0;
