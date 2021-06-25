dde23e3b349175712244d9de5aedd9ba40f1d447
Maddy B <maddx@live.com.au>
2021-06-25 21:53:33 +0800

    Refactoring Statistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -60,7 +60,18 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+            return result;
         }
     }
     public class InMemoryBook : Book
@@ -107,56 +118,18 @@ namespace GradeBook
             }
         }
         public override event GradeAddedDelegate GradeAdded;
-// Add statistics class identifier (return an object of type statistics)
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
-// for loop - (initializar a variable; condition to check before the runtime enters the loop; operation to perform after finishing the loop each time)
             for(var index = 0; index < grades.Count; index += 1)
             {   
-
-                if(grades[index] == 42.1)
-                {
-                    // break; - stop/leave loop
-                    // continue; - stop iteration and start next iteration
-                }
-                result.Low = Math.Min(grades[index], result.Low); 
-                result.High = Math.Max(grades[index], result.High);
-                result.Average += grades[index];
-            };
-            //Compute an Average/highest/lowest for the number stored in the Grades List
-            result.Average /= grades.Count;
-            switch(result.Average)
-            {
-                case var d when d >= 90.0:
-                result.Letter = 'A';
-                break;
-
-                case var d when d >= 80.0:
-                result.Letter = 'B';
-                break;
-
-                case var d when d >= 70.0:
-                result.Letter = 'C';
-                break;
-
-                case var d when d >= 60.0:
-                result.Letter = 'D';
-                break;
-
-                default:
-                result.Letter = 'F';
-                break;
+                result.Add(grades[index]);
             }
+   
             return result;
 
         }
-
-        // State - Add a field - to access grades 
         private List<double> grades;
         public const string CATEGORY = "Science";
     }
